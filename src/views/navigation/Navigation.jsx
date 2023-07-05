968eff64ae64cb28284b897d3b9531b8af7b6263
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2023-07-05 12:37:59 +0100

    Made links to send to a place on the page
    Made the navigation links send the user to the part of the page in question

--- src/views/navigation/Navigation.jsx
+++ src/views/navigation/Navigation.jsx
@@ -1,6 +1,18 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
+  // Function to move the viewpoint to the projects when the link is clicked
+  
+  const handlePortfolioClick = () => {
+    const element = document.getElementById('portfolio-link');
+    if (element) { element.scrollIntoView({ behavior: 'smooth' }); }
+  }
+
+  const handleContactClick = () => {
+    const element = document.getElementById('contact');
+    if (element) { element.scrollIntoView({ behavior: 'smooth' }); }
+  }
+
   return ( 
         <header>
     {/* // <!-- SIDE NAVBAR for screens over M--> */}
@@ -13,10 +25,7 @@ const Navigation = () => {
                 <NavLink to="/about-me">About Me</NavLink>
               </div>
               <div class="my-portfolio">
-                <a href="#portfolio-link">My Portfolio</a>
-              </div>
-              <div class="my-portfolio">
-                <NavLink to="./#portfolio-link">My PortfolioR</NavLink>
+                <NavLink to="./#portfolio-link" onClick={handlePortfolioClick}>My Portfolio</NavLink>
               </div>
               {/* <div class="coding-examples">
                 <a href="coding-examples.html">Coding Examples</a>
@@ -26,8 +35,12 @@ const Navigation = () => {
               </div> */}
             </div>
             <div class="contact-me">
-              <a href="#contact">Contact Me</a>
+                <NavLink to="/" onClick={handleContactClick}>Contact Me</NavLink>
             </div>
+
+            {/* <div class="contact-me">
+              <a href="#contact">Contact Me</a>
+            </div> */}
             <div class="social-media">
               <div class="social-links">
                   <div class="gap"></div>
