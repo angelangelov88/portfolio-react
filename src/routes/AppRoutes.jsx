e9ec79e672a096ebb29a8e69539f6a77064264bd
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2023-07-05 18:04:11 +0100

    Added error page
    Added not found page when not existing route is typed

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from '../views/navigation/Navigation';
 import Portfolio from '../views/portfolio/Portfolio';
 import AboutMe from '../views/aboutMe/AboutMe';
+import Error from '../views/error/Error';
 
 
 const AppRoutes = () => {
@@ -11,6 +12,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path='/' element={<Portfolio />} />
         <Route path='/about-me' element={<AboutMe />} />
+        <Route path='*' element={<Error />} />
       </Routes>
   </>     
 );
---
+++ src/views/error/Error.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Error = () => {
+  return ( 
+    <div style={{ "margin-left": "20%"}}>
+      <h2>Error 404</h2>
+      <h2>Page not found</h2>
+      <h3>Go to the <Link to="/">Homepage</Link></h3>
+    </div>
+   );
+}
+ 
+export default Error;
