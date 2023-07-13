94c34c0745fb7e73404c72f9dfe1906b6f98d98a
angelangelov88 <82048582+angelangelov88@users.noreply.github.com>
2023-07-13 08:51:34 +0100

    Revert "Updates"
    This reverts commit ddceb88d6d97503d348037a07f73d7dc77fe6b63.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/navigation/Navigation.jsx
@@ -0,0 +1,124 @@
+import React, { useRef } from "react";
+import { NavLink } from "react-router-dom";
+
+function Navigation() {
+  // Function to move the viewpoint to the projects when the link is clicked
+  const handlePortfolioClick = () => {
+    const element = document.getElementById("portfolio-link");
+    if (element) { element.scrollIntoView({ behavior: "smooth" }); }
+  };
+
+  const handleContactClick = () => {
+    const element = document.getElementById("contact");
+    if (element) { element.scrollIntoView({ behavior: "smooth" }); }
+  };
+
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+  const windowWidth = useRef(window.innerWidth);
+
+  const toggleNavBar = () => {
+    if (windowWidth?.current < 768) {
+      setNavbarOpen(!navbarOpen);
+    }
+  };
+
+  // Effect to set navbarOpen to false when user resizes screen
+  React.useEffect(() => {
+    const handleResize = () => {
+      windowWidth.current = window.innerWidth;
+      if (windowWidth.current > 768) {
+        setNavbarOpen(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <header>
+      {/* // <!-- SIDE NAVBAR for screens over M--> */}
+      <nav
+        className="navbar"
+        style={{ display: navbarOpen ? "block" : "none", }}
+      >
+        <div className="logo">
+          <NavLink to="/">A</NavLink>
+        </div>
+        <div className="navbar-items">
+          <div className="about-me">
+            <NavLink to="/about-me">About Me</NavLink>
+          </div>
+          <div className="my-portfolio">
+            <NavLink to="./#portfolio-link" onClick={handlePortfolioClick}>My Portfolio</NavLink>
+          </div>
+          {/* <div className="coding-examples">
+                <a href="coding-examples.html">Coding Examples</a>
+              </div> */}
+          {/* <div className="scs">
+                <a href="scs-scheme.html">SCS Scheme</a>
+              </div> */}
+        </div>
+        <div className="contact-me">
+          <NavLink to="/" onClick={handleContactClick}>Contact Me</NavLink>
+        </div>
+
+        {/* <div className="contact-me">
+              <a href="#contact">Contact Me</a>
+            </div> */}
+        <div className="social-media">
+          <div className="social-links">
+            <div className="gap" />
+            <a className="github" href="https://github.com/angelangelov88" target="_blank" rel="noreferrer">
+              <i className="fab fa-github social-logo" />
+            </a>
+            <div className="gap" />
+            <a className="linkedin" href="https://www.linkedin.com/in/ang-angelov/" target="_blank" rel="noreferrer">
+              <i className="fab fa-linkedin-in social-logo" />
+            </a>
+            {/* <div className="gap"></div>
+                  <a className="facebook" href="https://www.facebook.com/ang.angelov88" target="_blank" rel="noreferrer">
+                    <i className="fab fa-facebook-f social-logo"></i>
+                  </a>
+                    <div className="gap"></div>
+                  <a className="twitter" href="https://twitter.com/Angel69010637" target="_blank" rel="noreferrer">
+                    <i className="fab fa-twitter social-logo"></i>
+                  </a> */}
+          </div>
+        </div>
+      </nav>
+
+      {/* Small hamburger style navbar for small screens */}
+      {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+      <div
+        className="blocker"
+        style={{ display: navbarOpen ? "block" : "none", }}
+        onClick={toggleNavBar}
+        onKeyDown={toggleNavBar}
+        role="button"
+        tabIndex={0}
+      />
+
+      <div
+        className="hamburger"
+        style={{
+          display: windowWidth < 768 && navbarOpen ? "block" : windowWidth < 768 && !navbarOpen ? "none" : windowWidth > 768 && "block",
+          cursor: "pointer",
+        }}
+        onClick={toggleNavBar}
+        onKeyDown={toggleNavBar}
+        role="button"
+        tabIndex={0}
+      >
+        <div id="menu-lines">
+          <div className="menu-line" />
+          <div className="menu-line" />
+          <div className="menu-line" />
+        </div>
+        <p className="hamburger-title">MENU</p>
+      </div>
+    </header>
+
+  );
+}
+
+export default Navigation;
